af4da07e3de8227026e4ea8b6e9ab76e0a779807
Eddie Wang <eddie@eddiewang.me>
2015-04-05 07:28:42 -0400

    first commit

---
+++ TwitterScrape/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for TwitterScrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'TwitterScrape'
+
+SPIDER_MODULES = ['TwitterScrape.spiders']
+NEWSPIDER_MODULE = 'TwitterScrape.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'TwitterScrape (+http://www.yourdomain.com)'
